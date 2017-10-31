95b3c112aeea78377873c384f3718682521cfcd4
zhangolove <cl46@rice.edu>
2017-10-30 23:13:50 -0400

    investigate the errors induced by rolling

---
+++ test/validate_dataframe.py
@@ -0,0 +1,18 @@
+import pandas as pd
+import pandas_validator as pv
+
+# class SampleDataFrameValidator(pv.DataFrameValidator):
+#     row_num = 5
+#     column_num = 2
+#     label1 = pv.IntegerColumnValidator('label1', min_value=0, max_value=10)
+#     label2 = pv.FloatColumnValidator('label2', min_value=0, max_value=10)
+
+# validator = SampleDataFrameValidator()
+
+store = pd.HDFStore('../output/store.h5')
+df = store['cu1703_20170103_20170106']
+store.close()
+
+# https://stackoverflow.com/questions/15771472/pandas-rolling-mean-by-time-interval
+print("df index is unique {}".format(df.index.is_unique))
+print(df.rolling(window='6000s').mean())
